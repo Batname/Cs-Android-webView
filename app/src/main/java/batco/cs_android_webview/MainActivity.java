02399fa416da2c3f78654ed3ccc95430121d4b90
Batname <dadubinin@gmail.com>
2016-03-27 14:14:24 +0200

    add local storage

--- app/src/main/java/batco/cs_android_webview/MainActivity.java
+++ app/src/main/java/batco/cs_android_webview/MainActivity.java
@@ -2,6 +2,7 @@ package batco.cs_android_webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -11,10 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         WebView webview = new WebView(this);
-        // webview.loadUrl("http://10.0.2.2:8000"); // Origin origin 'http://10.0.2.2:8000'
+//        webview.loadUrl("http://10.0.2.2:8000"); // Origin origin 'http://10.0.2.2:8000'
         webview.loadUrl("https://www.casinoheroes.com");
         webview.setWebViewClient(new WebViewClient());
-        webview.getSettings().setJavaScriptEnabled(true);
+        WebSettings settings = webview.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
         setContentView(webview);
     }
 }
